acc0ff92e4a16b3fe2cf60d00f63ad8c9db92099
polinabuslovaa <103866597+polinabuslovaa@users.noreply.github.com>
2025-11-27 16:30:51 -0500

    Update fitness-app.py

--- fitness-app.py
+++ fitness-app.py
@@ -1,1 +1,24 @@
-print("hello world");
+# FITNESS AND DIET TRACKER
+
+import json # Store data in a text file for later access
+import os # Computers file system
+import requests # The requests library for APIs 
+
+
+
+def menu(): # Menu function to list the features 
+
+  print("Welcome to your fitness and diet tracker.") # Print statements 
+  print("1. Add type of workout ")
+  print("2. Add meal ")
+  print("3. Add steps ")
+  print("4. View History ")
+
+def workoutType():
+  workout = input("Enter the muscle group you would like excersizes for (eg: bicep, glutes, quads): ")
+def mealType(data):
+def steps(data):
+def history(data):
+
+  
+  
